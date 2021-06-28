d1a5f67549b20c10d635e3aa85d489f4b4a652b4
Sumanth <sumanth.l@testyantra.com>
2021-06-28 15:14:50 +0530

    Added main method into DeleteInvoice test script

--- Vtiger_CRM/src/test/java/invoice/DeleteInvoice.java
+++ Vtiger_CRM/src/test/java/invoice/DeleteInvoice.java
@@ -1,5 +1,12 @@
 package invoice;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class DeleteInvoice {
 
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "./src/test/resources/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+	}
 }
